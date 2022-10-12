f546a1554708a143f5d2c77917028317573076b1
muhanad abukar <muhanadabdullahiabukar@gmail.com>
2022-10-12 21:22:40 +0200

    compacted now

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -3,15 +3,10 @@ int help(int n, int x)
 {
 
     if (x == n)
-    {
-        return 1;
-    }
-    else
-    {
-        x++;
-        Console.WriteLine(x);
-        return help(n, x);
-    }
+        return n;
+    x++;
+    Console.WriteLine(x);
+    return help(n, x);
 }
 help(10, 0);
 
@@ -19,15 +14,9 @@ help(10, 0);
 int help2(int n)
 {
     if (n == 1)
-    {
-        Console.WriteLine(n);
-        return 1;
-    }
-    else
-    {
-        Console.WriteLine(n);
-        return help2(n - 1);
-    }
+        return n;
+    Console.WriteLine(n);
+    return help2(n - 1);
 }
 help2(10);
 
@@ -35,44 +24,32 @@ help2(10);
 int sum(int n)
 {
     if (n != 0)
-    {
-        return n + sum(n-1);
-    }
-    else
-    {
-        return n;
-    }
+        return n + sum(n - 1);
+
+    return n;
+
 }
 Console.WriteLine(sum(5));
 
 //oppgv 4
-string ret(string inp,int rec)
+string ret(string inp, int rec)
 {
     if (rec == inp.Length)
-    {
         return inp;
-    }
-    else
-    {
-        Console.WriteLine(inp[rec]);
-        rec++;
-        return inp + ret(inp, rec);
-    }
+    Console.WriteLine(inp[rec]);
+    rec++;
+    return inp + ret(inp, rec);
 }
 ret("123456789", 0);
 
 // ooppgv 5
-int help3(int n,int rec)
+int help3(int n, int rec)
 {
     if (n.ToString().Length == 1)
-    {
-        return rec+1;
-    }
-    else
-    {
-        var x = int.Parse(n.ToString().Substring(0, n.ToString().Length - 1));
-        rec++;
-        return help3(x,rec);
-    }
+        return rec + 1;
+
+    var x = int.Parse(n.ToString().Substring(0, n.ToString().Length - 1));
+    rec++;
+    return help3(x, rec);
 }
-Console.WriteLine(help3(150,0));
+Console.WriteLine(help3(150, 0));
